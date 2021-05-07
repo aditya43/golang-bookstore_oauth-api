1280aceaa77cba23decd9a196e2cbe89a9f9cabe
Aditya Hajare <aditya43@gmail.com>
2021-05-07 07:30:53 +0530

    Get Cassandra session

--- src/client/cassandra/cassandra_db.go
+++ src/client/cassandra/cassandra_db.go
@@ -1,19 +1,18 @@
 package cassandra
 
 import (
-	"fmt"
-
 	"github.com/gocql/gocql"
 )
+
+var cluster *gocql.ClusterConfig
 
 func init() {
 	// Connect to Cassandra cluster
 	cluster := gocql.NewCluster("127.0.0.1")
 	cluster.Keyspace = "oauth"
-	session, err := cluster.CreateSession()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected to Cassandra cluster successfully..")
-	defer session.Close()
+	cluster.Consistency = gocql.Quorum
+}
+
+func GetSession() (*gocql.Session, error) {
+	return cluster.CreateSession()
 }
