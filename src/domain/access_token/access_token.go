f2780d91413df136217b489f39482ace43787cd8
Aditya Hajare <aditya43@gmail.com>
2021-05-08 21:15:35 +0530

    Multiple grant types for access token

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -1,13 +1,47 @@
 package access_token
 
 import (
+	"fmt"
 	"strings"
 	"time"
 
+	"github.com/aditya43/golang-bookstore_oauth-api/src/utils/crypto_utils"
 	"github.com/aditya43/golang-bookstore_oauth-api/src/utils/errors"
 )
 
-const expirationTime = 24
+const (
+	expirationTime             = 24
+	grantTypePassword          = "password"
+	grantTypeClientCredentials = "client_credentials"
+)
+
+type AccessTokenRequest struct {
+	GrantType string `json:"grant_type"`
+	Scope     string `json:"scope"`
+
+	// Used for grant_type="password"
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	// Used for grant_type="client_credentials"
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (atR *AccessTokenRequest) Validate() *errors.RESTErr {
+	switch atR.GrantType {
+	case grantTypeClientCredentials:
+		break
+
+	case grantTypePassword:
+		break
+
+	default:
+		return errors.BadRequestErr("Invalid grant_type parameter")
+	}
+
+	return nil
+}
 
 type AccessToken struct {
 	AccessToken string `json:"access_token"`
@@ -37,12 +71,17 @@ func (at *AccessToken) Validate() *errors.RESTErr {
 	return nil
 }
 
-func GetNewAccessToken() AccessToken {
+func GetNewAccessToken(userId int64) AccessToken {
 	return AccessToken{
+		UserId:  userId,
 		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
 	}
 }
 
 func (accessToken AccessToken) IsExpired() bool {
 	return time.Unix(accessToken.Expires, 0).Before(time.Now().UTC())
+}
+
+func (accessToken *AccessToken) Generate() {
+	accessToken.AccessToken = crypto_utils.GetMd5(fmt.Sprintf("at-%d-%d-ran", accessToken.UserId, accessToken.Expires))
 }
