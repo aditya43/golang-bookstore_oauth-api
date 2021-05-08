7706b274f2e8eefeeb75f3a6ea71c7a4d07b54d0
Aditya Hajare <aditya43@gmail.com>
2021-05-08 21:16:22 +0530

    Test get new access token | reafctor

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -12,7 +12,7 @@ func TestAccessTokenConstants(t *testing.T) {
 }
 
 func TestGetNewAccessToken(t *testing.T) {
-	at := GetNewAccessToken()
+	at := GetNewAccessToken(0)
 
 	assert.False(t, at.IsExpired(), "Brand new access token should not be expired")
 
